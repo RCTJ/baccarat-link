42c2c171a3ca83f465ee9af3503a00829e4095b3
SBOAAAA <fun_pu@hotmail.com>
2022-11-11 13:20:34 +0700

    edit

---
+++ spec/baccarat-link-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BaccaratLinkView from '../lib/baccarat-link-view';
+
+describe('BaccaratLinkView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
